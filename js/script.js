7d786b65f0d3a6acc80aea71c0dfb24da2577730
wpdenham <wpdenham@gmail.com>
2018-03-27 20:14:13 -0500

    Began working on modal window for product items.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function viewProduct(id){
+	var isHidden = document.getElementById(id);
+	isHidden.classList.toggle("modal");
+	};
+
+function closeProduct(id){
+	var isHidden = document.getElementById(id);
+	isHidden.classList.toggle("modal");
+	};
