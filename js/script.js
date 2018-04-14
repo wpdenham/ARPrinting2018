8dd3fa22387c675af8c75fa8f97e9025d02ef181
wpdenham <wpdenham@gmail.com>
2018-04-14 13:10:14 -0500

    Added markup, styling and animation for -Additional Print Services- button to display list items on click.

--- js/script.js
+++ js/script.js
@@ -7,3 +7,13 @@ function closeProduct(id){
 	var isHidden = document.getElementById(id);
 	isHidden.classList.toggle("modal");
 	};
+
+function expandServices(){
+	const printServices = document.querySelector('.printservices');
+	
+	const addPrintServices = document.getElementById('addPrintServices');
+	
+	addPrintServices.addEventListener('click', function(){
+		printServices.classList.toggle('serviceshidden')});
+	
+}
